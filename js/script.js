ab749e48776b348eab18d95ffe61944f0676d3a6
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-06-09 17:15:26 +0000

    did assignment 6

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Lamees Hemdan rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lamees Hemdan
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+const getFoxImage = async (URLAddress) => {
+  try {
+    const response = await fetch(URLAddress);
+    const json = await response.json();
+    document.getElementById("fox").innerHTML= "<img src=" + json.image + ">"
+  }catch (err) {
+    console.log(err);
+  }
+}
+
+getFoxImage ("https://randomfox.ca/floof/")
